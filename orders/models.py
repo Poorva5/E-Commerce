5260c34edbafd4b73887d08c35fe1fba1e630171
Poorva5 <92199944+Poorva5@users.noreply.github.com>
2023-03-15 23:01:23 +0530

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -25,8 +25,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
-    
-    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE,null=True, blank=True)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     quantity = models.PositiveIntegerField(default=1, null=True, blank=True)
 
